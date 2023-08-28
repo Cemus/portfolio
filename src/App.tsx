a84cc6b999ac028e4b1bd45c2743461b3534d39f
Cemus <24947582+Cemus@users.noreply.github.com>
2023-08-28 14:19:08 +0200

    Delete Wave.tsx import

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,10 @@ import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Projects from "./components/Sections/Projects";
 import About from "./components/Sections/About";
-import Wave from "./components/Wave";
 import Footer from "./components/Footer";
 import Contacts from "./components/Sections/Contacts";
 
 function App() {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-      }
-    });
-  });
   return (
     <div className=" bg-gradient-to-r bg-dShade">
       <Header />
